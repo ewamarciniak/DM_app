05aa422cd9d1d051e95c0185794a110043dc78dc
Ewa Marciniak <emarciniak78@gmail.com>
2014-07-18 14:04:47 +0100

    First commit including postgres database setup and perpetuity gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DataMapperApp::Application.config.secret_token = '9dc5a54cc0ad2ae454d73cc8b42ace0af6010a4cdba00876870ee5d03c97bf290cfc6a0db3252f8d436befaa2db46652a2434919388b067b0b7fb859d3066885'
